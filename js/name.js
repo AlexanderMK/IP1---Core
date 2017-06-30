dedb761c89cabcb890de57a7cf91ebca459ddd65
Alexander <mburukungu@gmail.com>
2017-06-30 12:44:51 +0300

    initialized npm

--- js/name.js
+++ js/name.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('#username').submit(function(event){
     event.preventDefault();
     var name = $('#name').val();
-    $('#signup').hide();
+  
+    $('#username').hide();
     $('#solution').prepend('<p>Thank you, ' + name + ' has been added to our list!</p>');
   });
 });
